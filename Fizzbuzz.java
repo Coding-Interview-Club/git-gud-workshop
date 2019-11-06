2bb13d446f36d9066114131082c43534ce22787c
enoumy <enoumy@gmail.com>
2019-11-05 19:19:07 -0700

    Added fizz buzz code to improve :D

---
+++ Fizzbuzz.java
@@ -0,0 +1,21 @@
+class Fizzbuzz {
+    // If x is divisible by 2, print "fizz"
+    // If x is divisible by 3, print "buzz"
+    // If it's divisible by both print fizzbuzz
+    public void fizzBuzz(int n) {
+        for (int i = 1; i <= n; i++) {
+
+            if (i % 3 == 0) {
+              System.out.println("Fizz");
+            }
+
+            if (i % 5 == 0) {
+              System.out.println("Buzz");
+            }
+
+            if ((i % 3 != 0) && (i % 5 !=)) {
+              System.out.println(i);
+            }
+        }
+    }
+}

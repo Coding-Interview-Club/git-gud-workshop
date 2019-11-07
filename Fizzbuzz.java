4d3edd1b697dee467e698d9f1608be0a5c3f9c0b
enoumy <enoumy@gmail.com>
2019-11-07 16:40:44 -0700

    Refactored printing

--- Fizzbuzz.java
+++ Fizzbuzz.java
@@ -1,36 +1,37 @@
 class Fizzbuzz {
   // I am on the testing branch
-      // If x is divisible by 2, print "fizz"
-      // If x is divisible by 3, print "buzz"
-      // If it's divisible by both, print x
-      public static void fizzBuzz(int n) {
-          for (int i = 1; i <= n; i++) {
+    // If x is divisible by 2, print "fizz"
+    // If x is divisible by 3, print "buzz"
+    // If it's divisible by both, print x
+    public static void fizzBuzz(int n) {
+        for (int i = 1; i <= n; i++) {
 
-            if (isXMultipleOfY(i, 3) == true)
-            {
-              // Prints fizz
-              System.out.println("Fizz");
-            }
-
-                if(isXMultipleOfY(i, 5) == true){
-                  // Prints buzz
-                  System.out.println("Buzz");
-                }
-
-            if ((isXMultipleOfY(i, 3) == true) && (isXMultipleOfY(i, 5) == true)) {
-              System.out.println(i); // prints i
-            }
+          if (isXMultipleOfY(i, 3) == true)
+          {
+            // Prints fizz
+            System.out.print("Fizz");
           }
-      }
 
-      public static boolean isXMultipleOfY(int x, int y) {
-          for (int i = 0; i <= x; i += y)
-            if (i == x)
-                return true;
-          return false;
-      }
+              if(isXMultipleOfY(i, 5) == true){
+                // Prints buzz
+                System.out.print("Buzz");
+              }
 
-      public static void main(String[] args) {
-          fizzBuzz(20);
-      }
+          if ((isXMultipleOfY(i, 3) == true) && (isXMultipleOfY(i, 5) == true)) {
+            System.out.println(i); // prints i
+          }
+          System.out.println();
+        }
+    }
+
+    public static boolean isXMultipleOfY(int x, int y) {
+        for (int i = 0; i <= x; i += y)
+          if (i == x)
+              return true;
+        return false;
+    }
+
+    public static void main(String[] args) {
+        fizzBuzz(20);
+    }
 }

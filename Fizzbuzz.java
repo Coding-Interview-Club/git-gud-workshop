393a3e55ba7d921fd04cc5fb42cec321fa186e33
jesseolivas <jesseolivas@live.com>
2019-11-07 19:01:14 -0700

    Added comments, fixed indents

--- Fizzbuzz.java
+++ Fizzbuzz.java
@@ -1,39 +1,47 @@
 class Fizzbuzz {
-  // I am on the testing branch
-    // If x is divisible by 2, print "fizz"
-    // If x is divisible by 3, print "buzz"
-    // If it's divisible by both, print x
-    public static void fizzBuzz(int n) {
-        for (int i = 1; i <= n; i++) {
+    // I am on the testing branch - jesse
 
-          if((isXMultipleOfY(i, 3) == true) && (isXMultipleOfY(i, 5) == true))
-          {
-            // Prints fizz
-            System.out.println("FizzBuzz");
-          }
+    // Method to check values at index for divisibility
+    public static void fizzBuzz(int n){
 
-          else if(isXMultipleOfY(i, 5) == true){
-                // Prints buzz
-                System.out.println("Buzz");
-              }
+      for (int i = 1; i <= n; i++) {
 
-          else if(isXMultipleOfY(i, 3)== true){
-            System.out.println("Fizz"); // prints i
-          }
-          else{
-            System.out.println(i);
-          }
+        // If it's divisible by both, print "FizzBuzz"
+        if ((multXY(i, 3) == true) && (multXY(i, 5) == true)){
+          // Prints FizzBuzz
+          System.out.println("FizzBuzz");
         }
+
+        // If x is divisible by 3, print "Buzz"
+        else if (multXY(i, 5) == true){
+          // Prints Buzz
+          System.out.println("Buzz");
+        }
+
+        // If x is divisible by 2, print "Fizz"
+        else if (multXY(i, 3)== true){
+          //Prints Fizz
+          System.out.println("Fizz"); 
+        }
+
+        // If neither, print index value
+        else{
+          // prints i
+          System.out.println(i);
+        }
+      }
     }
 
-    public static boolean isXMultipleOfY(int x, int y) {
+    // Method to check divisibility 
+    public static boolean multXY(int x, int y) {
         for (int i = 0; i <= x; i += y)
           if (i == x)
               return true;
         return false;
     }
 
+    // Calls Fizzbuzz using given total index
     public static void main(String[] args) {
-        fizzBuzz(20);
+        fizzBuzz(69);
     }
 }

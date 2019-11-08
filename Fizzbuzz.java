58a06af226c47f4e5ca4ba95d59efd39812329fd
sroblesher <sroblesher1@miners.utep.edu>
2019-11-07 19:39:31 -0600

    Deleted the method and changed the inside of the for loop

--- Fizzbuzz.java
+++ Fizzbuzz.java
@@ -1,39 +1,20 @@
 class Fizzbuzz {
-  // I am on the testing branch
-    // If x is divisible by 2, print "fizz"
-    // If x is divisible by 3, print "buzz"
-    // If it's divisible by both, print x
+
     public static void fizzBuzz(int n) {
         for (int i = 1; i <= n; i++) {
-
-          if((isXMultipleOfY(i, 3) == true) && (isXMultipleOfY(i, 5) == true))
-          {
-            // Prints fizz
-            System.out.println("FizzBuzz");
-          }
-
-          else if(isXMultipleOfY(i, 5) == true){
-                // Prints buzz
-                System.out.println("Buzz");
-              }
-
-          else if(isXMultipleOfY(i, 3)== true){
-            System.out.println("Fizz"); // prints i
-          }
-          else{
-            System.out.println(i);
-          }
+            if (i%3 == 0 || i%5 == 0){
+                if (i%3 == 0)
+                  System.out.print("Fizz");
+                if (i%5 == 0)
+                  System.out.print("Buzz");
+                System.out.println();
+            }
+            else
+              System.out.println(i);
         }
     }
 
-    public static boolean isXMultipleOfY(int x, int y) {
-        for (int i = 0; i <= x; i += y)
-          if (i == x)
-              return true;
-        return false;
-    }
-
     public static void main(String[] args) {
-        fizzBuzz(20);
+        fizzBuzz(30);
     }
 }

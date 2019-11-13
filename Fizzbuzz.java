8f85c277bab51a1bce9e8112eacfbf30450e7925
jesseolivas <jesseolivas@live.com>
2019-11-12 18:25:26 -0700

    cleaned up, practicing git

--- Fizzbuzz.java
+++ Fizzbuzz.java
@@ -1,47 +1,32 @@
 class Fizzbuzz {
-    // I am on the testing branch - jesse
+       // I am on the testing branch - jesse
 
-    // Method to check values at index for divisibility
+    
     public static void fizzBuzz(int n){
-
+      // Iterate through all values up to highest number given
       for (int i = 1; i <= n; i++) {
 
-        // If it's divisible by both, print "FizzBuzz"
-        if ((multXY(i, 3) == true) && (multXY(i, 5) == true)){
-          // Prints FizzBuzz
+        // If it's divisible by both 3 and 5, print "FizzBuzz"
+        if(i % 3 == 0 && i % 5 == 0){
           System.out.println("FizzBuzz");
         }
-
-        // If x is divisible by 3, print "Buzz"
-        else if (multXY(i, 5) == true){
-          // Prints Buzz
+        // If divisible by 3, print "Fizz"
+        else if(i % 3 == 0){
+          System.out.println("Fizz");
+        }
+        // If divisible by 5, print "Buzz"
+        else if(i % 5 == 0){
           System.out.println("Buzz");
         }
-
-        // If x is divisible by 2, print "Fizz"
-        else if (multXY(i, 3)== true){
-          //Prints Fizz
-          System.out.println("Fizz"); 
-        }
-
-        // If neither, print index value
+        // All other cases, print i
         else{
-          // prints i
           System.out.println(i);
         }
       }
     }
-
-    // Method to check divisibility 
-    public static boolean multXY(int x, int y) {
-        for (int i = 0; i <= x; i += y)
-          if (i == x)
-              return true;
-        return false;
-    }
-
     // Calls Fizzbuzz using given total index
     public static void main(String[] args) {
         fizzBuzz(69);
     }
+
 }
